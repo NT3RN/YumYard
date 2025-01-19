b12591c4a6636082c9f9b55da0f196af3b42385b
nt3rn <98659883+NT3RN@users.noreply.github.com>
2025-01-20 03:44:33 +0600

    customerInfo update page database connection successfull

--- Customer/CustomerInfoUpdate.Designer.cs
+++ Customer/CustomerInfoUpdate.Designer.cs
@@ -29,7 +29,130 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CustomerInfoUpdate));
+            this.lblUserID = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblCurrentPass = new System.Windows.Forms.Label();
+            this.lblNewPass = new System.Windows.Forms.Label();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.tbUserID = new System.Windows.Forms.TextBox();
+            this.tbUserName = new System.Windows.Forms.TextBox();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.tbGender = new System.Windows.Forms.TextBox();
+            this.tbCurrPass = new System.Windows.Forms.TextBox();
+            this.tbNewPass = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // lblUserID
+            // 
+            this.lblUserID.AutoSize = true;
+            this.lblUserID.BackColor = System.Drawing.Color.Transparent;
+            this.lblUserID.Font = new System.Drawing.Font("Lucida Sans Unicode", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserID.Location = new System.Drawing.Point(338, 107);
+            this.lblUserID.Name = "lblUserID";
+            this.lblUserID.Size = new System.Drawing.Size(84, 23);
+            this.lblUserID.TabIndex = 1;
+            this.lblUserID.Text = "User ID";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.BackColor = System.Drawing.Color.Transparent;
+            this.lblName.Font = new System.Drawing.Font("Lucida Sans Unicode", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(338, 166);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(121, 23);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "User Name";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.BackColor = System.Drawing.Color.Transparent;
+            this.lblEmail.Font = new System.Drawing.Font("Lucida Sans Unicode", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(338, 225);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(119, 23);
+            this.lblEmail.TabIndex = 3;
+            this.lblEmail.Text = "User Email";
+            // 
+            // lblCurrentPass
+            // 
+            this.lblCurrentPass.AutoSize = true;
+            this.lblCurrentPass.BackColor = System.Drawing.Color.Transparent;
+            this.lblCurrentPass.Font = new System.Drawing.Font("Lucida Sans Unicode", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCurrentPass.Location = new System.Drawing.Point(338, 343);
+            this.lblCurrentPass.Name = "lblCurrentPass";
+            this.lblCurrentPass.Size = new System.Drawing.Size(192, 23);
+            this.lblCurrentPass.TabIndex = 4;
+            this.lblCurrentPass.Text = "Current Password";
+            // 
+            // lblNewPass
+            // 
+            this.lblNewPass.AutoSize = true;
+            this.lblNewPass.BackColor = System.Drawing.Color.Transparent;
+            this.lblNewPass.Font = new System.Drawing.Font("Lucida Sans Unicode", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNewPass.Location = new System.Drawing.Point(338, 402);
+            this.lblNewPass.Name = "lblNewPass";
+            this.lblNewPass.Size = new System.Drawing.Size(157, 23);
+            this.lblNewPass.TabIndex = 5;
+            this.lblNewPass.Text = "New Password";
+            // 
+            // lblGender
+            // 
+            this.lblGender.AutoSize = true;
+            this.lblGender.BackColor = System.Drawing.Color.Transparent;
+            this.lblGender.Font = new System.Drawing.Font("Lucida Sans Unicode", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGender.Location = new System.Drawing.Point(338, 284);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(84, 23);
+            this.lblGender.TabIndex = 6;
+            this.lblGender.Text = "Gender";
+            // 
+            // tbUserID
+            // 
+            this.tbUserID.Enabled = false;
+            this.tbUserID.Font = new System.Drawing.Font("Lucida Sans Unicode", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbUserID.Location = new System.Drawing.Point(564, 110);
+            this.tbUserID.Name = "tbUserID";
+            this.tbUserID.ReadOnly = true;
+            this.tbUserID.Size = new System.Drawing.Size(183, 24);
+            this.tbUserID.TabIndex = 7;
+            // 
+            // tbUserName
+            // 
+            this.tbUserName.Location = new System.Drawing.Point(564, 169);
+            this.tbUserName.Name = "tbUserName";
+            this.tbUserName.Size = new System.Drawing.Size(183, 20);
+            this.tbUserName.TabIndex = 8;
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Location = new System.Drawing.Point(564, 228);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(183, 20);
+            this.tbEmail.TabIndex = 9;
+            // 
+            // tbGender
+            // 
+            this.tbGender.Location = new System.Drawing.Point(564, 287);
+            this.tbGender.Name = "tbGender";
+            this.tbGender.Size = new System.Drawing.Size(183, 20);
+            this.tbGender.TabIndex = 10;
+            // 
+            // tbCurrPass
+            // 
+            this.tbCurrPass.Location = new System.Drawing.Point(564, 346);
+            this.tbCurrPass.Name = "tbCurrPass";
+            this.tbCurrPass.Size = new System.Drawing.Size(183, 20);
+            this.tbCurrPass.TabIndex = 11;
+            // 
+            // tbNewPass
+            // 
+            this.tbNewPass.Location = new System.Drawing.Point(564, 407);
+            this.tbNewPass.Name = "tbNewPass";
+            this.tbNewPass.Size = new System.Drawing.Size(183, 20);
+            this.tbNewPass.TabIndex = 12;
             // 
             // CustomerInfoUpdate
             // 
@@ -37,15 +160,41 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Wheat;
             this.ClientSize = new System.Drawing.Size(1056, 594);
+            this.Controls.Add(this.tbNewPass);
+            this.Controls.Add(this.tbCurrPass);
+            this.Controls.Add(this.tbGender);
+            this.Controls.Add(this.tbEmail);
+            this.Controls.Add(this.tbUserName);
+            this.Controls.Add(this.tbUserID);
+            this.Controls.Add(this.lblGender);
+            this.Controls.Add(this.lblNewPass);
+            this.Controls.Add(this.lblCurrentPass);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblUserID);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "CustomerInfoUpdate";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "CustomerInfoUpdate";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblUserID;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblCurrentPass;
+        private System.Windows.Forms.Label lblNewPass;
+        private System.Windows.Forms.Label lblGender;
+        private System.Windows.Forms.TextBox tbUserID;
+        private System.Windows.Forms.TextBox tbUserName;
+        private System.Windows.Forms.TextBox tbEmail;
+        private System.Windows.Forms.TextBox tbGender;
+        private System.Windows.Forms.TextBox tbCurrPass;
+        private System.Windows.Forms.TextBox tbNewPass;
     }
 }
--- Customer/CustomerInfoUpdate.cs
+++ Customer/CustomerInfoUpdate.cs
@@ -7,14 +7,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
+using YumYard.DatabaseAccess;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 
 namespace YumYard.Customer
 {
     public partial class CustomerInfoUpdate : Form
     {
-        public CustomerInfoUpdate()
+        public CustomerInfoUpdate(string email)
         {
             InitializeComponent();
+
+            
+            LoadCustomerData(email);
+        }
+        private void LoadCustomerData(string userEmail) 
+        {
+            try
+            {
+                string query = $"SELECT C_ID, C_Name, C_Email, C_Gender FROM Customer WHERE C_Email = '{userEmail}'";
+                string error;
+                var customerData = DbAccess.GetData(query, out error);
+                if (!string.IsNullOrEmpty(error))
+                {
+                    MessageBox.Show("Oops! Something went wrong: " + error);
+                    return;
+                }
+
+                if (customerData.Rows.Count > 0)
+                {
+                    tbUserID.Text = customerData.Rows[0]["C_ID"].ToString();
+                    tbUserName.Text = customerData.Rows[0]["C_Name"].ToString();
+                    tbEmail.Text = customerData.Rows[0]["C_Email"].ToString();
+                    tbGender.Text = customerData.Rows[0]["C_Gender"].ToString();
+                    
+                }
+                else
+                {
+                    MessageBox.Show("No customer data found for the provided email.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred while loading customer data: " + ex.Message);
+            }
+
         }
     }
 }
