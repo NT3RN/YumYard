07b977c44e305007ae5d15dfa166b292cc9f1d30
nt3rn <98659883+NT3RN@users.noreply.github.com>
2025-02-03 00:21:30 +0600

    database connected to r1order

--- Rform/r1Order.Designer.cs
+++ Rform/r1Order.Designer.cs
@@ -28,22 +28,362 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.food1pic = new System.Windows.Forms.PictureBox();
+            this.food2pic = new System.Windows.Forms.PictureBox();
+            this.food3pic = new System.Windows.Forms.PictureBox();
+            this.food6pic = new System.Windows.Forms.PictureBox();
+            this.food5pic = new System.Windows.Forms.PictureBox();
+            this.food4pic = new System.Windows.Forms.PictureBox();
+            this.cbfood1 = new System.Windows.Forms.CheckBox();
+            this.cbfood2 = new System.Windows.Forms.CheckBox();
+            this.cbfood3 = new System.Windows.Forms.CheckBox();
+            this.cbfood6 = new System.Windows.Forms.CheckBox();
+            this.cbfood5 = new System.Windows.Forms.CheckBox();
+            this.cbfood4 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.tbPrice1 = new System.Windows.Forms.TextBox();
+            this.tbPrice2 = new System.Windows.Forms.TextBox();
+            this.tbPrice3 = new System.Windows.Forms.TextBox();
+            this.tbPrice6 = new System.Windows.Forms.TextBox();
+            this.tbPrice5 = new System.Windows.Forms.TextBox();
+            this.tbPrice4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.food1pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.food2pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.food3pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.food6pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.food5pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.food4pic)).BeginInit();
             this.SuspendLayout();
+            // 
+            // food1pic
+            // 
+            this.food1pic.BackgroundImage = global::YumYard.Properties.Resources.chefhead_ICON;
+            this.food1pic.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.food1pic.Location = new System.Drawing.Point(42, 33);
+            this.food1pic.Name = "food1pic";
+            this.food1pic.Size = new System.Drawing.Size(89, 76);
+            this.food1pic.TabIndex = 0;
+            this.food1pic.TabStop = false;
+            // 
+            // food2pic
+            // 
+            this.food2pic.BackgroundImage = global::YumYard.Properties.Resources.chefhead_ICON;
+            this.food2pic.Location = new System.Drawing.Point(216, 33);
+            this.food2pic.Name = "food2pic";
+            this.food2pic.Size = new System.Drawing.Size(89, 76);
+            this.food2pic.TabIndex = 1;
+            this.food2pic.TabStop = false;
+            // 
+            // food3pic
+            // 
+            this.food3pic.BackgroundImage = global::YumYard.Properties.Resources.chefhead_ICON;
+            this.food3pic.Location = new System.Drawing.Point(390, 33);
+            this.food3pic.Name = "food3pic";
+            this.food3pic.Size = new System.Drawing.Size(89, 76);
+            this.food3pic.TabIndex = 2;
+            this.food3pic.TabStop = false;
+            // 
+            // food6pic
+            // 
+            this.food6pic.BackgroundImage = global::YumYard.Properties.Resources.chefhead_ICON;
+            this.food6pic.Location = new System.Drawing.Point(390, 210);
+            this.food6pic.Name = "food6pic";
+            this.food6pic.Size = new System.Drawing.Size(89, 76);
+            this.food6pic.TabIndex = 5;
+            this.food6pic.TabStop = false;
+            // 
+            // food5pic
+            // 
+            this.food5pic.BackgroundImage = global::YumYard.Properties.Resources.chefhead_ICON;
+            this.food5pic.Location = new System.Drawing.Point(216, 210);
+            this.food5pic.Name = "food5pic";
+            this.food5pic.Size = new System.Drawing.Size(89, 76);
+            this.food5pic.TabIndex = 4;
+            this.food5pic.TabStop = false;
+            // 
+            // food4pic
+            // 
+            this.food4pic.BackgroundImage = global::YumYard.Properties.Resources.chefhead_ICON;
+            this.food4pic.Location = new System.Drawing.Point(42, 210);
+            this.food4pic.Name = "food4pic";
+            this.food4pic.Size = new System.Drawing.Size(89, 76);
+            this.food4pic.TabIndex = 3;
+            this.food4pic.TabStop = false;
+            // 
+            // cbfood1
+            // 
+            this.cbfood1.AutoSize = true;
+            this.cbfood1.Location = new System.Drawing.Point(42, 115);
+            this.cbfood1.Name = "cbfood1";
+            this.cbfood1.Size = new System.Drawing.Size(64, 19);
+            this.cbfood1.TabIndex = 6;
+            this.cbfood1.Text = "cbfood";
+            this.cbfood1.UseVisualStyleBackColor = true;
+            // 
+            // cbfood2
+            // 
+            this.cbfood2.AutoSize = true;
+            this.cbfood2.Location = new System.Drawing.Point(216, 115);
+            this.cbfood2.Name = "cbfood2";
+            this.cbfood2.Size = new System.Drawing.Size(84, 19);
+            this.cbfood2.TabIndex = 7;
+            this.cbfood2.Text = "checkBox1";
+            this.cbfood2.UseVisualStyleBackColor = true;
+            // 
+            // cbfood3
+            // 
+            this.cbfood3.AutoSize = true;
+            this.cbfood3.Location = new System.Drawing.Point(390, 115);
+            this.cbfood3.Name = "cbfood3";
+            this.cbfood3.Size = new System.Drawing.Size(84, 19);
+            this.cbfood3.TabIndex = 8;
+            this.cbfood3.Text = "checkBox2";
+            this.cbfood3.UseVisualStyleBackColor = true;
+            // 
+            // cbfood6
+            // 
+            this.cbfood6.AutoSize = true;
+            this.cbfood6.Location = new System.Drawing.Point(390, 304);
+            this.cbfood6.Name = "cbfood6";
+            this.cbfood6.Size = new System.Drawing.Size(84, 19);
+            this.cbfood6.TabIndex = 11;
+            this.cbfood6.Text = "checkBox3";
+            this.cbfood6.UseVisualStyleBackColor = true;
+            // 
+            // cbfood5
+            // 
+            this.cbfood5.AutoSize = true;
+            this.cbfood5.Location = new System.Drawing.Point(216, 304);
+            this.cbfood5.Name = "cbfood5";
+            this.cbfood5.Size = new System.Drawing.Size(84, 19);
+            this.cbfood5.TabIndex = 10;
+            this.cbfood5.Text = "checkBox4";
+            this.cbfood5.UseVisualStyleBackColor = true;
+            // 
+            // cbfood4
+            // 
+            this.cbfood4.AutoSize = true;
+            this.cbfood4.Location = new System.Drawing.Point(42, 304);
+            this.cbfood4.Name = "cbfood4";
+            this.cbfood4.Size = new System.Drawing.Size(84, 19);
+            this.cbfood4.TabIndex = 9;
+            this.cbfood4.Text = "checkBox5";
+            this.cbfood4.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.comboBox1.Location = new System.Drawing.Point(42, 140);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(83, 23);
+            this.comboBox1.TabIndex = 12;
+            this.comboBox1.Text = "1";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.comboBox2.Location = new System.Drawing.Point(213, 140);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(83, 23);
+            this.comboBox2.TabIndex = 13;
+            this.comboBox2.Text = "1";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.comboBox3.Location = new System.Drawing.Point(390, 140);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(83, 23);
+            this.comboBox3.TabIndex = 14;
+            this.comboBox3.Text = "1";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.comboBox4.Location = new System.Drawing.Point(42, 329);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(83, 23);
+            this.comboBox4.TabIndex = 15;
+            this.comboBox4.Text = "1";
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.comboBox5.Location = new System.Drawing.Point(213, 329);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(83, 23);
+            this.comboBox5.TabIndex = 16;
+            this.comboBox5.Text = "1";
+            // 
+            // comboBox6
+            // 
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.comboBox6.Location = new System.Drawing.Point(390, 329);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(83, 23);
+            this.comboBox6.TabIndex = 17;
+            this.comboBox6.Text = "1";
+            // 
+            // tbPrice1
+            // 
+            this.tbPrice1.Enabled = false;
+            this.tbPrice1.Location = new System.Drawing.Point(42, 169);
+            this.tbPrice1.Name = "tbPrice1";
+            this.tbPrice1.Size = new System.Drawing.Size(87, 24);
+            this.tbPrice1.TabIndex = 18;
+            // 
+            // tbPrice2
+            // 
+            this.tbPrice2.Enabled = false;
+            this.tbPrice2.Location = new System.Drawing.Point(213, 169);
+            this.tbPrice2.Name = "tbPrice2";
+            this.tbPrice2.Size = new System.Drawing.Size(87, 24);
+            this.tbPrice2.TabIndex = 19;
+            // 
+            // tbPrice3
+            // 
+            this.tbPrice3.Enabled = false;
+            this.tbPrice3.Location = new System.Drawing.Point(390, 169);
+            this.tbPrice3.Name = "tbPrice3";
+            this.tbPrice3.Size = new System.Drawing.Size(87, 24);
+            this.tbPrice3.TabIndex = 20;
+            // 
+            // tbPrice6
+            // 
+            this.tbPrice6.Enabled = false;
+            this.tbPrice6.Location = new System.Drawing.Point(390, 358);
+            this.tbPrice6.Name = "tbPrice6";
+            this.tbPrice6.Size = new System.Drawing.Size(87, 24);
+            this.tbPrice6.TabIndex = 23;
+            // 
+            // tbPrice5
+            // 
+            this.tbPrice5.Enabled = false;
+            this.tbPrice5.Location = new System.Drawing.Point(213, 358);
+            this.tbPrice5.Name = "tbPrice5";
+            this.tbPrice5.Size = new System.Drawing.Size(87, 24);
+            this.tbPrice5.TabIndex = 22;
+            // 
+            // tbPrice4
+            // 
+            this.tbPrice4.Enabled = false;
+            this.tbPrice4.Location = new System.Drawing.Point(42, 358);
+            this.tbPrice4.Name = "tbPrice4";
+            this.tbPrice4.Size = new System.Drawing.Size(87, 24);
+            this.tbPrice4.TabIndex = 21;
             // 
             // r1Order
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.ClientSize = new System.Drawing.Size(837, 450);
+            this.Controls.Add(this.tbPrice6);
+            this.Controls.Add(this.tbPrice5);
+            this.Controls.Add(this.tbPrice4);
+            this.Controls.Add(this.tbPrice3);
+            this.Controls.Add(this.tbPrice2);
+            this.Controls.Add(this.tbPrice1);
+            this.Controls.Add(this.comboBox6);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cbfood6);
+            this.Controls.Add(this.cbfood5);
+            this.Controls.Add(this.cbfood4);
+            this.Controls.Add(this.cbfood3);
+            this.Controls.Add(this.cbfood2);
+            this.Controls.Add(this.cbfood1);
+            this.Controls.Add(this.food6pic);
+            this.Controls.Add(this.food5pic);
+            this.Controls.Add(this.food4pic);
+            this.Controls.Add(this.food3pic);
+            this.Controls.Add(this.food2pic);
+            this.Controls.Add(this.food1pic);
             this.Font = new System.Drawing.Font("Lucida Sans Unicode", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "r1Order";
             this.Text = "r1Order";
+            ((System.ComponentModel.ISupportInitialize)(this.food1pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.food2pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.food3pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.food6pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.food5pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.food4pic)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox food1pic;
+        private System.Windows.Forms.PictureBox food2pic;
+        private System.Windows.Forms.PictureBox food3pic;
+        private System.Windows.Forms.PictureBox food6pic;
+        private System.Windows.Forms.PictureBox food5pic;
+        private System.Windows.Forms.PictureBox food4pic;
+        private System.Windows.Forms.CheckBox cbfood1;
+        private System.Windows.Forms.CheckBox cbfood2;
+        private System.Windows.Forms.CheckBox cbfood3;
+        private System.Windows.Forms.CheckBox cbfood6;
+        private System.Windows.Forms.CheckBox cbfood5;
+        private System.Windows.Forms.CheckBox cbfood4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.ComboBox comboBox6;
+        private System.Windows.Forms.TextBox tbPrice1;
+        private System.Windows.Forms.TextBox tbPrice2;
+        private System.Windows.Forms.TextBox tbPrice3;
+        private System.Windows.Forms.TextBox tbPrice6;
+        private System.Windows.Forms.TextBox tbPrice5;
+        private System.Windows.Forms.TextBox tbPrice4;
     }
 }
--- Rform/r1Order.cs
+++ Rform/r1Order.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using YumYard.DatabaseAccess;
 
 namespace YumYard.Rform
 {
@@ -16,11 +17,69 @@ namespace YumYard.Rform
         public string Uid { get; set; }
         private string rId;
         public string Rid { get; set; }
+
+        private string f1;
+        private string f2;
+        private string f3;
+        private string f4;
+        private string f5;
+        private string f6;
+        private double fp1;
+        private double fp2;
+        private double fp3;
+        private double fp4;
+        private double fp5;
+        private double fp6;
+
         public r1Order(string uid, string rid)
         {
             InitializeComponent();
             this.Uid = uid;
             this.Rid = rid;
+            LoadProductData();
+        }
+
+        private void LoadProductData()
+        {
+            try
+            {
+                string query = $"SELECT TOP 6 ProductName, ProductPrice FROM RestaurantProducts WHERE RestaurantID = {Rid}";
+                string error;
+                DataTable productData = DbAccess.GetData(query, out error);
+
+                if (!string.IsNullOrEmpty(error))
+                {
+                    MessageBox.Show("Error loading product data: " + error);
+                    return;
+                }
+
+                if (productData.Rows.Count > 0)
+                {
+                    if (productData.Rows.Count > 0) { f1 = productData.Rows[0]["ProductName"].ToString(); fp1 = Convert.ToDouble(productData.Rows[0]["ProductPrice"]); }
+                    if (productData.Rows.Count > 1) { f2 = productData.Rows[1]["ProductName"].ToString(); fp2 = Convert.ToDouble(productData.Rows[1]["ProductPrice"]); }
+                    if (productData.Rows.Count > 2) { f3 = productData.Rows[2]["ProductName"].ToString(); fp3 = Convert.ToDouble(productData.Rows[2]["ProductPrice"]); }
+                    if (productData.Rows.Count > 3) { f4 = productData.Rows[3]["ProductName"].ToString(); fp4 = Convert.ToDouble(productData.Rows[3]["ProductPrice"]); }
+                    if (productData.Rows.Count > 4) { f5 = productData.Rows[4]["ProductName"].ToString(); fp5 = Convert.ToDouble(productData.Rows[4]["ProductPrice"]); }
+                    if (productData.Rows.Count > 5) { f6 = productData.Rows[5]["ProductName"].ToString(); fp6 = Convert.ToDouble(productData.Rows[5]["ProductPrice"]); }
+
+                    cbfood1.Text = f1;
+                    cbfood2.Text = f2;
+                    cbfood3.Text = f3;
+                    cbfood4.Text = f4;
+                    cbfood5.Text = f5;
+                    cbfood6.Text = f6;
+                    tbPrice1.Text = fp1.ToString();
+                    tbPrice2.Text = fp2.ToString();
+                    tbPrice3.Text = fp3.ToString();
+                    tbPrice4.Text = fp4.ToString();
+                    tbPrice5.Text = fp5.ToString();
+                    tbPrice6.Text = fp6.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred while loading product data: " + ex.Message);
+            }
         }
     }
 }
