38c5dbff6b9dd9b30e5adbaeb88ac1f2c9a9be19
nt3rn <98659883+NT3RN@users.noreply.github.com>
2025-02-02 22:29:25 +0600

    user order res

--- Customer/ResturantPicker.cs
+++ Customer/ResturantPicker.cs
@@ -8,18 +8,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using YumYard.DatabaseAccess;
 using YumYard.Register___Login;
+using YumYard.Restaurants;
 
 namespace YumYard.Customer
 {
     public partial class ResturantPicker : Form
     {
         private string userEmail;
+        private string userID;
+        private int resID;
+        //restaurant names
+        public string res1Name;
+        public string res2Name;
+        public string res3Name;
+        public string res4Name;
+        
         public ResturantPicker(string email)
         {
             InitializeComponent();
             userEmail = email;
+            LoadCustomerData(email);
 
+        }
+
+        private void LoadCustomerData(string userEmail)
+        {
+            try
+            {
+                string query = $"SELECT C_ID FROM Customer WHERE C_Email = '{userEmail}'";
+                string error;
+                var customerData = DbAccess.GetData(query, out error);
+                if (!string.IsNullOrEmpty(error))
+                {
+                    MessageBox.Show("Oops! Something went wrong: " + error);
+                    return;
+                }
+
+                if (customerData.Rows.Count > 0)
+                {
+                    userID = customerData.Rows[0]["C_ID"].ToString();
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred while loading customer data: " + ex.Message);
+            }
         }
 
         private void msiProfile_Click(object sender, EventArgs e)
@@ -40,6 +76,15 @@ namespace YumYard.Customer
             }
             
             
+        }
+
+        private void btnResturant1_Click(object sender, EventArgs e)
+        {
+            res1Name = btnResturant1.Text;
+            resID = 1;
+            Res1 res1 = new Res1(userEmail, userID, res1Name, resID);
+            res1.Show();
+            this.Hide();
         }
     }
 }
---
+++ Rform/r1Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YumYard.Rform
+{
+    public partial class r1Order : Form
+    {
+        private string uId;
+        public string Uid { get; set; }
+        private string rId;
+        public string Rid { get; set; }
+        public r1Order(string uid, string rid)
+        {
+            InitializeComponent();
+            this.Uid = uid;
+            this.Rid = rid;
+        }
+    }
+}
